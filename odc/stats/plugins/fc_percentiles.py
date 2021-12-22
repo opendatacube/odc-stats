58abb4af32527f7397438ac9d1a857d57f6d4d80
Alex Leith <alexgleith@gmail.com>
2021-12-22 12:07:10 +1100

    FC Percentiles Changes - Fix counts, make sum > 120 NODATA and clip to 0-100 (#15)
    * Fix counts in FCP
    
    * Increment version
    
    * Implement filtering of pixels that sum to greater than 120
    
    * Implement clipping from 0-100
    
    * Fix up count logic

--- odc/stats/plugins/fc_percentiles.py
+++ odc/stats/plugins/fc_percentiles.py
@@ -47,27 +47,59 @@ class StatsFCP(StatsPluginInterface):
         water = xx.water & 0b1110_1111
         xx = xx.drop_vars(["water"])
 
+        # Pick out the dry pixels
+        dry = water == 0
+
+        # Pick out the pixels that have an unmixing error of less than 30
         unmixing_error_lt_30 = xx.ue < 30
         xx = xx.drop_vars(["ue"])
 
-        dry = water == 0
-        dry_and_ue_lt_30 = dry & unmixing_error_lt_30
+        # Sum the bands and pick out only pixels that sum to less than 120
+        # Also clips to 0-100
+        sum_bands = None
+        for band in xx.data_vars.keys():
+            attributes = xx[band].attrs
+            mask = xx[band] == NODATA
+            band_data = keep_good_only(xx[band], ~mask, nodata=0)
+            if sum_bands is None:
+                sum_bands = band_data
+            else:
+                sum_bands = sum_bands + band_data
 
-        xx = keep_good_only(xx, dry_and_ue_lt_30, nodata=NODATA)
+            # Note: clipping to 0-100
+            clipped = np.clip(xx[band], 0, 100)
+            # Set masked values back to 255
+            xx[band] = clipped.where(~mask, NODATA)
+            xx[band].attrs = attributes
+
+        # Note: hard limit of 120 for the sum of values.
+        sum_lt_120 = sum_bands < 120
+
+        valid = dry & unmixing_error_lt_30 & sum_lt_120
+
+        xx = keep_good_only(xx, valid, nodata=NODATA)
+
         xx["wet"] = water == 128
+        xx["valid"] = valid
 
         return xx
 
     def fuser(self, xx):
         wet = xx["wet"]
-        xx = _xr_fuse(xx.drop_vars(["wet"]), partial(_fuse_mean_np, nodata=NODATA), "")
+        valid = xx["valid"]
 
-        band, *bands = xx.data_vars.keys()
-        all_bands_invalid = xx[band] == NODATA
-        for band in bands:
-            all_bands_invalid &= xx[band] == NODATA
+        xx = _xr_fuse(xx.drop_vars(["wet", "valid"]), partial(_fuse_mean_np, nodata=NODATA), "")
 
-        xx["wet"] = _xr_fuse(wet, _fuse_or_np, wet.name) & all_bands_invalid
+        # Not sure why we're doing this... alex leith, 2021-12
+        # band, *bands = xx.data_vars.keys()
+        # all_bands_nodata = xx[band] == NODATA
+        # for band in bands:
+        #     all_bands_nodata &= xx[band] == NODATA
+
+        # This used to have "& all_bands_nodata"
+        xx["wet"] = _xr_fuse(wet, _fuse_or_np, wet.name)
+        xx["valid"] = _xr_fuse(valid, _fuse_or_np, valid.name)
+
         return xx
 
     def reduce(self, xx: xr.Dataset) -> xr.Dataset:
@@ -76,7 +108,8 @@ class StatsFCP(StatsPluginInterface):
         # all_bands_valid => 2
 
         wet = xx["wet"]
-        xx = xx.drop_vars(["wet"])
+        valid = xx["valid"]
+        xx = xx.drop_vars(["wet", "valid"])
 
         yy = xr_quantile_bands(xx, [0.1, 0.5, 0.9], nodata=NODATA)
         is_ever_wet = _or_fuser(wet).squeeze(wet.dims[0], drop=True)
@@ -89,7 +122,8 @@ class StatsFCP(StatsPluginInterface):
         all_bands_valid = all_bands_valid.astype(np.uint8)
         is_ever_wet = is_ever_wet.astype(np.uint8)
         yy["qa"] = 1 + all_bands_valid - is_ever_wet * (1 - all_bands_valid)
-        yy["count_valid"] = all_bands_valid
+
+        yy["count_valid"] = valid.sum(axis=0, dtype="int16")
 
         return yy
 
