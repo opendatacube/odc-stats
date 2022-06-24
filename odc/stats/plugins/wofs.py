dc834c2ce6287ddf93489ee57cba68162d7881c0
Emma Ai <emma.ai@ga.gov.au>
2022-06-24 04:49:52 +0000

    fix wofs plugin with dilation and mask pre-reproject

--- odc/stats/plugins/wofs.py
+++ odc/stats/plugins/wofs.py
@@ -3,23 +3,32 @@ Water Observations Summaries
 
 Water Observations Summaries are made up of:
 
-- `count_clear`: a count of every time a pixel was observed (not obscured by terrain or clouds)
+- `count_clear`: a count of every time a pixel was observed
+  (not obscured by terrain or clouds)
 - `count_wet`: a count of every time a pixel was observed and wet
 - `frequency`: what fraction of time (wet/clear) was the pixel wet
 
 The counts are stored as `int16` and the frequency as `float32`.
 
-There are two different Stats Plugin classes implemented in this module. The first generates summary data from
-individual water observations, and the second generates a summary of summaries, which is used when generating an all
+There are two different Stats Plugin classes implemented in this module.
+The first generates summary data from individual water observations,
+and the second generates a summary of summaries, which is used when generating an all
 of time summary from existing annual summaries.
 
 """
-from typing import Sequence, Tuple
+
+from typing import Tuple, Dict, Iterable
 import numpy as np
 import xarray as xr
-from datacube.model import Dataset
-from datacube.utils.geometry import GeoBox
-from odc.algo import safe_div, apply_numexpr, keep_good_only, binary_dilation
+from odc.algo import (
+    safe_div,
+    apply_numexpr,
+    keep_good_only,
+    mask_cleanup,
+    _xr_fuse,
+    _fuse_and_np,
+    _fuse_or_np,
+)
 from ._registry import StatsPluginInterface, register
 
 
@@ -27,7 +36,8 @@ class StatsWofs(StatsPluginInterface):
     """
     Generate a Summary of Water Observations data from individual observations
 
-    The summary is made up of counts of visible and visible and wet, and the frequency of visible and wet.
+    The summary is made up of counts of visible and visible and wet,
+    and the frequency of visible and wet.
 
     Output data types are:
     - `count_clear`: `int16`
@@ -42,12 +52,15 @@ class StatsWofs(StatsPluginInterface):
     VERSION = "1.6.0"
     PRODUCT_FAMILY = "wo_summary"
 
-    # these get padded out if dilation was requested
-    BAD_BITS_MASK = 0b0110_1000  # Cloud/Shadow + Terrain Shadow
+    # these get padded out if cloud_filter is not None
+    BAD_BITS_MASK = dict(cloud=(1 << 6), cloud_shadow=(1 << 5), terrain_shadow=(1 << 3))
 
-    def __init__(self, dilation: int = 0, **kwargs):
+    def __init__(
+        self, cloud_filters: Dict[str, Iterable[Tuple[str, int]]] = None, **kwargs
+    ):
         super().__init__(input_bands=["water"], **kwargs)
-        self._dilation = dilation  # number of pixels to pad around BAD pixels
+        # dilation scheme for cloud/shadow
+        self.cloud_filters = cloud_filters if cloud_filters is not None else {}
 
     @property
     def measurements(self) -> Tuple[str, ...]:
@@ -75,22 +88,35 @@ class StatsWofs(StatsPluginInterface):
 
         out:
           .bad<Bool>   - pixel should not be counted
-          .some<Bool>  - there is data (bad or good but not nodata)
+          .some<Bool>  - there is data (x.water & 0b11) == 0,
+                         to distinguish "count=0" resulted from "nodata"
+                         or "not up to the classification criteria"
           .dry<Bool>   - pixel has dry classification and is not ``bad``
           .wet<Bool>   - pixel has wet classification and is not ``bad``
         """
-        if self._dilation != 0:
-            xx["bad"] = binary_dilation(
-                (xx.water & self.BAD_BITS_MASK) > 0, self._dilation
-            ) | ((xx.water & 0b0111_1110) > 0)
-        else:
-            xx["bad"] = (xx.water & 0b0111_1110) > 0
 
-        # some = (x.water&3)==0, i.e. nodata==0 and non_contigous==0
-        xx["some"] = apply_numexpr("((water<<30)>>30)==0", xx, name="some")
-        xx["dry"] = xx.water == 0
-        xx["wet"] = xx.water == 128
-        xx = xx.drop_vars("water")
+        xx["bad"] = apply_numexpr(
+            "(water & (~(1<<7))) > 0", xx, dtype="bool", name="bad"
+        )
+        xx["some"] = apply_numexpr(
+            "((water<<30)>>30)==0", xx, dtype="bool", name="some"
+        )
+
+        # dilate both 'some' and 'bad'
+        for key, val in self.BAD_BITS_MASK.items():
+            if self.cloud_filters.get(key) is not None:
+                raw_mask = (xx["water"] & val) > 0
+                raw_mask = mask_cleanup(
+                    raw_mask, mask_filters=self.cloud_filters.get(key)
+                )
+                xx["some"] |= raw_mask
+                xx["bad"] |= raw_mask
+
+        xx["dry"] = apply_numexpr("(water==0) & (~bad)", xx, dtype="bool", name="dry")
+        xx["wet"] = apply_numexpr(
+            "(water==(1<<7)) & (~bad)", xx, dtype="bool", name="wet"
+        )
+        xx = xx.drop_vars(["water", "bad"])
         for dv in xx.data_vars.values():
             dv.attrs.pop("nodata", None)
 
@@ -99,24 +125,19 @@ class StatsWofs(StatsPluginInterface):
     @staticmethod
     def fuser(xx):
         """
-        xx.bad  -- don't count
         xx.wet  -- is wet
         xx.dry  -- is dry
         xx.some -- there was at least one non-nodata observation at that pixel
         """
-        from odc.algo._masking import _or_fuser
+        # both wet = T => wet = T
+        # both dry = T => dry = T
+        # either some = T => some = T
 
-        # Merge everything with OR first
-        xx = _or_fuser(xx)
+        wet = _xr_fuse(xx["wet"], _fuse_and_np, "wet")
+        dry = _xr_fuse(xx["dry"], _fuse_and_np, "dry")
+        some = _xr_fuse(xx["some"], _fuse_or_np, "some")
 
-        # Ensure all 3 bits are exclusive
-        #  bad=T, wet=?, dry=? => (wet'=F  , dry'=F)
-        #  bad=F, wet=T, dry=T => (wet'=F  , dry'=F)
-        #  else                => (wet'=wet, dry'=dry)
-        wet = apply_numexpr("wet & (~dry) & (~bad)", xx, dtype="bool")
-        dry = apply_numexpr("dry & (~wet) & (~bad)", xx, dtype="bool")
-
-        return xr.Dataset(dict(wet=wet, dry=dry, bad=xx.bad, some=xx.some))
+        return xr.Dataset(dict(wet=wet, dry=dry, some=some))
 
     def reduce(self, xx: xr.Dataset) -> xr.Dataset:
         nodata = -999
@@ -156,8 +177,8 @@ class StatsWofsFullHistory(StatsPluginInterface):
     - `count_wet`: `int16`
     - `frequency`: `float32`
 
-    Special care is taken with no-data values, both to pass them through and when calculating the counts and
-    frequencies.
+    Special care is taken with no-data values, both to pass them through and
+    when calculating the counts and frequencies.
     """
 
     NAME = "ga_ls_wo_fq_myear_3"
@@ -184,8 +205,8 @@ class StatsWofsFullHistory(StatsPluginInterface):
         nodata = dtype.type(xx.count_clear.nodata)
 
         # `missing` is a record of all pixels that were never observed.
-        # Store it separately first, then substitute it back in after computing the counts and
-        # frequency.
+        # Store it separately first, then substitute it back in after
+        # computing the counts and frequency.
         missing = (xx.count_clear == xx.count_clear.nodata).all(axis=0)
         cc = apply_numexpr(
             "where(count_clear==nodata, 0, count_clear)",
@@ -213,7 +234,8 @@ class StatsWofsFullHistory(StatsPluginInterface):
             _nan=np.float32("nan"),
         )
 
-        # Finalise the *count* variables by re-inserting the no-data value based on `missing`
+        # Finalise the *count* variables by re-inserting
+        # the no-data value based on `missing`
         count_clear = apply_numexpr(
             "where(missing, nodata, cc)",
             _yy,
