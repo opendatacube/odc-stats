53bf74fd10cccb98e50898c20324fa8714823ede
Emma Ai <emma.ai@ga.gov.au>
2022-08-05 03:48:57 +0000

    add integration test to git workflow

---
+++ tests/integration_test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Test GeoMAD"
+
+odc-stats save-tasks --config https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/update_stats_configs/dev/services/odc-stats/geomedian/ga_ls8c_nbart_gm_cyear_3.yaml --year=2015 --overwrite geomad-eo3-test-run.db
+odc-stats run  --threads=1 --config https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/update_stats_configs/dev/services/odc-stats/geomedian/ga_ls8c_nbart_gm_cyear_3.yaml --location file:///tmp --overwrite geomad-eo3-test-run.db
+ls -alt /tmp
