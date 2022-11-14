4a169e68f437a6db1225cfeb1ae56982ef77cda7
Emma Ai <emma.ai@ga.gov.au>
2022-11-14 02:04:14 +0000

    patch the version with increment

--- scripts/patch_version.py
+++ scripts/patch_version.py
@@ -13,24 +13,27 @@ def match_version(line):
     return version_str
 
 
-def mk_dev_version(v, build_number):
-    fixed = version.parse(v).release
-    next_version = (*fixed, f"dev{build_number}")
-    return ".".join(map(str, next_version))
+def mk_dev_version(v, patch_number):
+    *fixed, patch = version.parse(v).release
+    if int(patch_number) < 0:
+        next_version = int(patch) + 1
+    else:
+        next_version = patch_number
+    return ".".join(map(str, fixed + [next_version]))
 
 
-def patch_version_lines(lines, build_number):
+def patch_version_lines(lines, patch_number):
     for line in lines:
         v_prev = match_version(line)
         if v_prev is not None:
-            v_next = mk_dev_version(v_prev, build_number)
+            v_next = mk_dev_version(v_prev, patch_number)
             line = line.replace(v_prev, v_next)
         yield line
 
 
-def patch_file(fname, build_number):
+def patch_file(fname, patch_number):
     with open(fname, "rt", encoding="utf-8") as src:
-        lines = list(patch_version_lines(src, build_number))
+        lines = list(patch_version_lines(src, patch_number))
     with open(fname, "wt", encoding="utf-8") as dst:
         dst.writelines(lines)
 
@@ -38,8 +41,8 @@ def patch_file(fname, build_number):
 if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) < 2:
-        print(f"Usage: {sys.argv[0]} build-number [FILE]...")
+        print(f"Usage: {sys.argv[0]} patch_number [FILE]...")
 
-    git_sha, *files = args
+    input_patch, *files = args
     for f in files:
-        patch_file(f, git_sha)
+        patch_file(f, input_patch)
