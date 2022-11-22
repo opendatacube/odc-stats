d5fff61ca2428c4a3c208281d69e2b8b58ab5c3a
Damien Ayers <damien@omad.net>
2022-11-22 11:15:33 +1100

    Linting fixes

--- odc/stats/plugins/__init__.py
+++ odc/stats/plugins/__init__.py
@@ -1,2 +1,9 @@
 from ._base import StatsPluginInterface
 from ._registry import resolve, import_all, register
+
+__all__ = [
+    "StatsPluginInterface",
+    "resolve",
+    "import_all",
+    "register",
+]
