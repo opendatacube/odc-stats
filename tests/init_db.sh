1f2f31d4fb035f3588dae60f569819425b3e541f
Sai Ma <supermarkioner@gmail.com>
2023-04-18 06:24:05 +0000

    Add the S2 GM to integration test case

--- tests/init_db.sh
+++ tests/init_db.sh
@@ -7,7 +7,7 @@ datacube system check
 # datacube product add https://raw.githubusercontent.com/digitalearthafrica/config/master/products/esa_s2_l2a.odc-product.yaml
 # stac-to-dc --bbox='4,5,5,6' --collections='sentinel-s2-l2a-cogs' --datetime='2019-09-01/2020-01-01' --limit 1000
 
-# GeoMAD
+# LS GeoMAD
 datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/product_metadata/eo3_landsat_ard.odc-type.yaml
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/baseline_satellite_data/c3/ga_ls8c_ard_3.odc-product.yaml
 # only index several data to speed up yearly summary run
@@ -16,7 +16,7 @@ s3-to-dc "s3://dea-public-data/baseline/ga_ls8c_ard_3/089/079/2015/03/*/*.json" 
 s3-to-dc "s3://dea-public-data/baseline/ga_ls8c_ard_3/089/079/2015/04/*/*.json" --no-sign-request --skip-lineage --stac ga_ls8c_ard_3
 s3-to-dc "s3://dea-public-data/baseline/ga_ls8c_ard_3/089/079/2015/05/*/*.json" --no-sign-request --skip-lineage --stac ga_ls8c_ard_3
 
-# WO
+# LS WO
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/inland_water/c3_wo/ga_ls_wo_3.odc-product.yaml
 
 # only index several data to speed up yearly summary run
@@ -25,7 +25,7 @@ s3-to-dc "s3://dea-public-data/derivative/ga_ls_wo_3/1-6-0/089/079/2015/03/*/*.j
 s3-to-dc "s3://dea-public-data/derivative/ga_ls_wo_3/1-6-0/089/079/2015/04/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_wo_3
 s3-to-dc "s3://dea-public-data/derivative/ga_ls_wo_3/1-6-0/089/079/2015/05/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_wo_3
 
-# FC 
+# LS FC 
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/land_and_vegetation/c3_fc/ga_ls_fc_3.odc-product.yaml
 
 # only index several data to speed up yearly summary run
@@ -33,3 +33,9 @@ s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-1/089/079/2015/02/*/*.j
 s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-1/089/079/2015/03/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
 s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-1/089/079/2015/04/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
 s3-to-dc "s3://dea-public-data/derivative/ga_ls_fc_3/2-5-1/089/079/2015/05/*/*.json" --no-sign-request --skip-lineage --stac ga_ls_fc_3
+
+# S2 GeoMAD
+datacube metadata add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/product_metadata/eo3_sentinel_ard.odc-type.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/baseline_satellite_data/c3/ga_s2am_ard_3.odc-product.yaml
+# only index several data to speed up yearly summary run
+s3-to-dc "s3://dea-public-data/baseline/ga_s2am_ard_3/55/HFB/2020/02/*/*/*.odc-metadata.yaml" --no-sign-request --skip-lineage ga_s2am_ard_3
