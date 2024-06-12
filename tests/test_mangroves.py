0aa9773651e04047083d7c1f3d9554fdd48f5a56
Emma Ai <emmaai@users.noreply.github.com>
2024-06-12 16:45:25 +0930

    Fix mangroves and wo plugins (#132)
    * fail mangroves if shapefile is not provided
    
    * set nodata for float type
    
    * mock mangrove extent shapefile
    
    ---------
    
    Co-authored-by: Emma Ai <emma.ai@ga.gov.au>

--- tests/test_mangroves.py
+++ tests/test_mangroves.py
@@ -1,8 +1,70 @@
 import numpy as np
 import xarray as xr
 import dask.array as da
+import os
 from odc.stats.plugins.mangroves import Mangroves
+import tempfile
+import json
+import fiona
+from fiona.crs import CRS
+from datacube.utils.geometry import GeoBox
+from affine import Affine
 import pytest
+
+
+@pytest.fixture
+def mangrove_shape():
+    data = """
+    {
+   "type":"FeatureCollection",
+   "features":[
+      {
+         "geometry":{
+            "type":"Polygon",
+            "coordinates":[
+               [
+                  [
+                     0,
+                     0
+                  ],
+                  [
+                     0,
+                     100
+                  ],
+                  [
+                     100,
+                     100
+                  ],
+                  [
+                     100,
+                     0
+                  ],
+                  [
+                     0,
+                     0
+                  ]
+               ]
+            ]
+         },
+         "type":"Feature"
+      }
+   ]
+}
+    """
+    data = json.loads(data)["features"][0]
+    tmpdir = tempfile.mkdtemp()
+    filename = os.path.join(tmpdir, "test.json")
+    with fiona.open(
+        filename,
+        "w",
+        driver="GeoJSON",
+        crs=CRS.from_epsg(3577),
+        schema={
+            "geometry": "Polygon",
+        },
+    ) as dst:
+        dst.write(data)
+    return filename
 
 
 @pytest.fixture
@@ -50,11 +112,15 @@ def dataset():
     band_3 = da.from_array(band_3, chunks=(1, -1, -1))
 
     index = [np.datetime64("2000-01-01T00")]
-    coords = {
-        "x": np.linspace(10, 20, band_1.shape[2]),
-        "y": np.linspace(0, 5, band_1.shape[1]),
-        "time": index,
-    }
+
+    affine = Affine.translation(10, 0) * Affine.scale(
+        (20 - 10) / band_1.shape[2], (5 - 0) / band_1.shape[1]
+    )
+    geobox = GeoBox(
+        crs="epsg:3577", affine=affine, width=band_1.shape[2], height=band_1.shape[1]
+    )
+    coords = geobox.xr_coords()
+    coords.update({"time": index})
 
     data_vars = {
         "pv_pc_10": xr.DataArray(
@@ -70,14 +136,14 @@ def dataset():
     return xx
 
 
-def test_native_transform(dataset):
-    mangroves = Mangroves()
+def test_native_transform(dataset, mangrove_shape):
+    mangroves = Mangroves(mangroves_extent=mangrove_shape)
     out_xx = mangroves.native_transform(dataset)
     assert (out_xx == dataset).all()
 
 
-def test_reduce(dataset):
-    mangroves = Mangroves()
+def test_reduce(dataset, mangrove_shape):
+    mangroves = Mangroves(mangroves_extent=mangrove_shape)
     yy = mangroves.reduce(dataset)
     expected_results = dataset.pv_pc_10.copy(True)
     expected_results.data = np.array(
