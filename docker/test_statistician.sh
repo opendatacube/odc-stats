82198f5d4798a83ef145afee44c4ccf041bd60ed
Emma Ai <emma.ai@ga.gov.au>
2024-08-06 10:17:36 +0000

    change docker-compose to docker compose

--- docker/test_statistician.sh
+++ docker/test_statistician.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-docker-compose up -d
+docker compose up -d
 sleep 5
 
-docker-compose exec -T stats odc-stats --version
+docker compose exec -T stats odc-stats --version
 echo "Indexing some data"
-docker-compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats ./tests/init_db.sh
+docker compose exec -e AWS_DEFAULT_REGION=ap-southeast-2 -T stats ./tests/init_db.sh
 echo "Data regression test"
-docker-compose exec -T stats  ./tests/integration_test.sh
+docker compose exec -T stats  ./tests/integration_test.sh
