dda76294f5553ec2f8bc820cf185252097a8d0d3
tebadi <toktam.ebadi@ga.gov.au>
2024-09-12 09:19:47 +1000

    Applied formatting.

--- odc/stats/plugins/lc_level3.py
+++ odc/stats/plugins/lc_level3.py
@@ -7,6 +7,7 @@ from ._registry import StatsPluginInterface, register
 
 NODATA = 255
 
+
 class StatsLccsLevel3(StatsPluginInterface):
     NAME = "ga_ls_lccs_level3"
     SHORT_NAME = NAME
@@ -23,30 +24,28 @@ class StatsLccsLevel3(StatsPluginInterface):
         l34_dss = xx.classes_l3_l4
         urban_dss = xx.urban_classes
         cultivated_dss = xx.cultivated_class
-        
-        cultivated_mask = l34_dss == 110 
+
+        cultivated_mask = l34_dss == 110
         l34_cultivated_masked = xr.where(cultivated_mask, cultivated_dss, l34_dss)
-        
+
         urban_mask = l34_dss == 210
         l34_urban_cultivated_masked = xr.where(urban_mask, urban_dss, l34_cultivated_masked)
-       
+
         attrs = xx.attrs.copy()
         attrs["nodata"] = int(NODATA)
         l34_urban_cultivated_masked = l34_urban_cultivated_masked.squeeze(dim=["spec"])
         dims = l34_urban_cultivated_masked.dims
-        
+
         data_vars = {
             "level3_class": xr.DataArray(
                 l34_urban_cultivated_masked.data, dims=dims, attrs=attrs
             )
         }
- 
+
         coords = dict((dim, xx.coords[dim]) for dim in dims)
         level3 = xr.Dataset(data_vars=data_vars, coords=coords, attrs=attrs)
 
         return level3
-       
-
 
 
 register("lccs_level3", StatsLccsLevel3)
