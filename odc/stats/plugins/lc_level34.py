7e5647605dcad7332477ea0d911c6746c0a46858
Emma Ai <emmaai@users.noreply.github.com>
2024-11-28 16:03:42 +1030

    classify landcover level34 by class def and code csv (#174)
    * fix typo on class code in landcover level4
    
    * classify landcover level34 by class def and code csv
    
    * remove unneeded test
    
    * add required package
    
    * move columns of dependencies into config
    
    * add tests for level34
    
    * allow test access remote csv
    
    * Add a unit test to cover all the classes at level4
    
    ---------
    
    Co-authored-by: Emma Ai <emma.ai@ga.gov.au>
    Co-authored-by: Toktam Ebadi <toktam.ebadi@ga.gov.au>

--- odc/stats/plugins/lc_level34.py
+++ odc/stats/plugins/lc_level34.py
@@ -2,29 +2,22 @@
 Plugin of Module A3 in LandCover PipeLine
 """
 
-from typing import Optional, List
+from typing import Optional, Dict, List
 
-import numpy as np
 import xarray as xr
+import s3fs
+import os
+import pandas as pd
+import dask.array as da
+import logging
 
 from ._registry import StatsPluginInterface, register
-from ._utils import rasterize_vector_mask
+from ._utils import rasterize_vector_mask, generate_numexpr_expressions
+from odc.stats._algebra import expr_eval
 from osgeo import gdal
 
-from .l34_utils import (
-    l4_water_persistence,
-    l4_veg_cover,
-    lc_level3,
-    l4_cultivated,
-    l4_natural_veg,
-    l4_natural_aquatic,
-    l4_surface,
-    l4_bare_gradation,
-    l4_water,
-)
-
-
 NODATA = 255
+_log = logging.getLogger(__name__)
 
 
 class StatsLccsLevel4(StatsPluginInterface):
@@ -35,15 +28,27 @@ class StatsLccsLevel4(StatsPluginInterface):
 
     def __init__(
         self,
+        class_def_path: str = None,
+        class_condition: Dict[str, List] = None,
         urban_mask: str = None,
         filter_expression: str = None,
         mask_threshold: Optional[float] = None,
-        veg_threshold: Optional[List] = None,
-        bare_threshold: Optional[List] = None,
-        watper_threshold: Optional[List] = None,
+        data_var_condition: Optional[Dict] = None,
         **kwargs,
     ):
         super().__init__(**kwargs)
+        if class_def_path is None:
+            raise ValueError("Missing level34 class definition csv")
+
+        if class_def_path.startswith("s3://"):
+            if not s3fs.S3FileSystem(anon=True).exists(class_def_path):
+                raise FileNotFoundError(f"{class_def_path} not found")
+        elif not os.path.exists(class_def_path):
+            raise FileNotFoundError(f"{class_def_path} not found")
+
+        if class_condition is None:
+            raise ValueError("Missing input to generate classification conditions")
+
         if urban_mask is None:
             raise ValueError("Missing urban mask shapefile")
 
@@ -54,33 +59,63 @@ class StatsLccsLevel4(StatsPluginInterface):
         if filter_expression is None:
             raise ValueError("Missing urban mask filter")
 
+        self.class_def = pd.read_csv(class_def_path)
+        self.class_condition = class_condition
+        cols = set()
+        for k, v in self.class_condition.items():
+            cols |= {k} | set(v)
+
+        self.class_def = self.class_def[list(cols)].astype(str).fillna("nan")
+
         self.urban_mask = urban_mask
         self.filter_expression = filter_expression
         self.mask_threshold = mask_threshold
-
-        self.veg_threshold = (
-            veg_threshold if veg_threshold is not None else [1, 4, 15, 40, 65, 100]
-        )
-        self.bare_threshold = bare_threshold if bare_threshold is not None else [20, 60]
-        self.watper_threshold = (
-            watper_threshold if watper_threshold is not None else [1, 4, 7, 10]
+        self.data_var_condition = (
+            {} if data_var_condition is None else data_var_condition
         )
 
     def fuser(self, xx):
         return xx
 
-    # pylint: disable=too-many-locals
-    def reduce(self, xx: xr.Dataset) -> xr.Dataset:
+    def classification(self, xx, class_def, con_cols, class_col):
+        expressions = generate_numexpr_expressions(
+            class_def[con_cols + [class_col]], class_col, "res"
+        )
+        local_dict = {
+            key: xx[self.data_var_condition.get(key, key)].data for key in con_cols
+        }
+        res = da.full(xx.level_3_4.shape, 0, dtype="uint8")
 
-        # Water persistence
-        water_persistence = l4_water_persistence.water_persistence(
-            xx, self.watper_threshold
+        for expression in expressions:
+            _log.debug(expression)
+            local_dict.update({"res": res})
+            res = expr_eval(
+                expression,
+                local_dict,
+                name="apply_rule",
+                dtype="uint8",
+            )
+
+        # This seems redundant while res can be init to NODATA above,
+        # but it's a point to sanity check no class is missed
+        res = expr_eval(
+            "where((a!=a)|(a>=_n), _n, b)",
+            {"a": xx.level_3_4.data, "b": res},
+            name="mark_nodata",
+            dtype="uint8",
+            **{"_n": NODATA},
         )
 
-        # #TODO WATER (99-104)
-        l4 = l4_water.water_classification(xx, water_persistence)
+        return res
 
-        # Generate Level3 classes
+    def reduce(self, xx: xr.Dataset) -> xr.Dataset:
+        class_col = "level3"
+        level3 = self.classification(
+            xx, self.class_def, self.class_condition[class_col], class_col
+        )
+
+        # apply urban mask
+        # 215 -> 216 if urban_mask == 0
         urban_mask = rasterize_vector_mask(
             self.urban_mask,
             xx.geobox.transform,
@@ -89,38 +124,33 @@ class StatsLccsLevel4(StatsPluginInterface):
             threshold=self.mask_threshold,
         )
 
-        level3 = lc_level3.lc_level3(xx, urban_mask)
-
-        # Vegetation cover
-        veg_cover = l4_veg_cover.canopyco_veg_con(xx, self.veg_threshold)
-
-        # Apply cultivated Level-4 classes (1-18)
-        l4 = l4_cultivated.lc_l4_cultivated(l4, level3, xx.woody, veg_cover)
-
-        # Apply terrestrial vegetation classes [19-36]
-        l4 = l4_natural_veg.lc_l4_natural_veg(l4, level3, xx.woody, veg_cover)
-
-        # Bare gradation
-        bare_gradation = l4_bare_gradation.bare_gradation(
-            xx, self.bare_threshold, veg_cover
+        level3 = expr_eval(
+            "where((a==215)&(b<1), 216, a)",
+            {"a": level3, "b": urban_mask},
+            name="mask_non_urban",
+            dtype="uint8",
         )
 
-        l4 = l4_natural_aquatic.natural_auquatic_veg(l4, veg_cover, xx.water_season)
-
-        level4 = l4_surface.lc_l4_surface(l4, level3, bare_gradation)
-
-        level3 = level3.astype(np.uint8)
-        level4 = level4.astype(np.uint8)
-
+        # append level3 to the input dataset so it can be used
+        # to classify level4
         attrs = xx.attrs.copy()
         attrs["nodata"] = NODATA
         dims = xx.level_3_4.dims[1:]
+        coords = dict((dim, xx.coords[dim]) for dim in dims)
+        xx["level3"] = xr.DataArray(
+            level3.squeeze(), dims=dims, attrs=attrs, coords=coords
+        )
+
+        class_col = "level4"
+        level4 = self.classification(
+            xx, self.class_def, self.class_condition[class_col], class_col
+        )
+
         data_vars = {
             k: xr.DataArray(v, dims=dims, attrs=attrs)
             for k, v in zip(self.measurements, [level3.squeeze(), level4.squeeze()])
         }
 
-        coords = dict((dim, xx.coords[dim]) for dim in dims)
         leve34 = xr.Dataset(data_vars=data_vars, coords=coords, attrs=xx.attrs)
         return leve34
 
---
+++ tests/test_lc_level34.py
@@ -0,0 +1,499 @@
+from odc.stats.plugins.lc_level34 import StatsLccsLevel4
+from odc.stats.plugins._utils import generate_numexpr_expressions
+
+import re
+import os
+import numpy as np
+import pandas as pd
+import xarray as xr
+import dask.array as da
+from datacube.utils.geometry import GeoBox
+from affine import Affine
+from unittest.mock import patch
+
+
+import pytest
+
+
+NODATA = 255
+
+
+@pytest.fixture(scope="module")
+def image_groups():
+    l34 = np.array(
+        [
+            [
+                [210, 210, 210],
+                [210, 210, 210],
+                [223, 210, 210],
+                [221, 221, 221],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    urban = np.array(
+        [
+            [
+                [216, 216, 215],
+                [216, 216, 216],
+                [215, 215, 215],
+                [215, 215, 215],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    woody = np.array(
+        [
+            [
+                [113, 113, 113],
+                [113, 113, 255],
+                [114, 114, 114],
+                [114, 114, 255],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    pv_pc_50 = np.array(
+        [
+            [
+                [1, 64, 65],
+                [66, 40, 41],
+                [3, 61, 78],
+                [4, 23, 42],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    bs_pc_50 = np.array(
+        [
+            [
+                [1, 64, NODATA],
+                [66, 40, 41],
+                [1, 40, 66],
+                [NODATA, 1, 42],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    cultivated = np.array(
+        [
+            [
+                [255, 255, 255],
+                [255, 255, 255],
+                [255, 255, 255],
+                [255, 255, 255],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    water_frequency = np.array(
+        [
+            [
+                [1, 3, 2],
+                [4, 5, 6],
+                [2, 2, 11],
+                [10, 11, 12],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    water_season = np.array(
+        [
+            [
+                [1, 2, 1],
+                [2, 1, 2],
+                [1, 1, 2],
+                [2, 2, 1],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    tuples = [
+        (np.datetime64("2000-01-01T00"), np.datetime64("2000-01-01")),
+    ]
+    index = pd.MultiIndex.from_tuples(tuples, names=["time", "solar_day"])
+
+    affine = Affine.translation(10, 0) * Affine.scale(
+        (20 - 10) / l34.shape[2], (5 - 0) / l34.shape[1]
+    )
+    geobox = GeoBox(
+        crs="epsg:3577", affine=affine, width=l34.shape[2], height=l34.shape[1]
+    )
+    coords = geobox.xr_coords()
+
+    data_vars = {
+        "level_3_4": xr.DataArray(
+            da.from_array(l34, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "artificial_surface": xr.DataArray(
+            da.from_array(urban, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "cultivated": xr.DataArray(
+            da.from_array(cultivated, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "woody": xr.DataArray(
+            da.from_array(woody, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "pv_pc_50": xr.DataArray(
+            da.from_array(pv_pc_50, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "bs_pc_50": xr.DataArray(
+            da.from_array(bs_pc_50, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "water_frequency": xr.DataArray(
+            da.from_array(water_frequency, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "water_season": xr.DataArray(
+            da.from_array(water_season, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+    }
+
+    xx = xr.Dataset(data_vars=data_vars, coords=coords)
+    xx = xx.assign_coords(xr.Coordinates.from_pandas_multiindex(index, "spec"))
+    return xx
+
+
+def test_l4_classes(image_groups, urban_shape):
+    expected_l3 = [[216, 216, 215], [216, 216, 216], [220, 215, 215], [220, 220, 220]]
+
+    expected_l4 = [[95, 97, 93], [97, 96, 96], [100, 93, 93], [101, 101, 101]]
+    with patch.dict(
+        os.environ,
+        {
+            "AWS_ACCESS_KEY_ID": "fake-access-key",
+            "AWS_SECRET_ACCESS_KEY": "fake-secret-key",
+            "AWS_SESSION_TOKEN": "fake-session-token",  # Optional
+        },
+    ):
+        stats_l4 = StatsLccsLevel4(
+            measurements=["level3", "level4"],
+            class_def_path="s3://dea-public-data-dev/lccs_validation/c3/data_to_plot/"
+            "lccs_colour_scheme_golden_dark_au_c3.csv",
+            class_condition={
+                "level3": ["level1", "artificial_surface", "cultivated"],
+                "level4": [
+                    "level1",
+                    "level3",
+                    "woody",
+                    "water_season",
+                    "water_frequency",
+                    "pv_pc_50",
+                    "bs_pc_50",
+                ],
+            },
+            data_var_condition={"level1": "level_3_4"},
+            urban_mask=urban_shape,
+            filter_expression="mock > 9",
+            mask_threshold=0.3,
+        )
+    ds = stats_l4.reduce(image_groups)
+
+    assert (ds.level3.compute() == expected_l3).all()
+    assert (ds.level4.compute() == expected_l4).all()
+
+
+@pytest.mark.parametrize(
+    "rules_df, expected_expressions",
+    [
+        # Test with range conditions
+        # when condition numbers are the same the order doesn't matter
+        (
+            pd.DataFrame(
+                {
+                    "condition_1": ["[5, 10)", "(1, 4]"],
+                    "condition_2": ["==2", "!=2"],
+                    "final_class": [1, 2],
+                }
+            ),
+            [
+                "where((condition_1>1.0)&(condition_1<=4.0)&(condition_2!=2.0), 2, previous)",
+                "where((condition_1>=5.0)&(condition_1<10.0)&(condition_2==2.0), 1, previous)",
+            ],
+        ),
+        # Test with NaN
+        # when clause with smaller number of conditions always takes precedence
+        (
+            pd.DataFrame(
+                {
+                    "condition_1": ["[5, 10)", "nan"],
+                    "condition_2": ["==2", "!=2"],
+                    "final_class": [1, 2],
+                }
+            ),
+            [
+                "where((condition_2!=2.0), 2, previous)",
+                "where((condition_1>=5.0)&(condition_1<10.0)&(condition_2==2.0), 1, previous)",
+            ],
+        ),
+        # Test with single value implying "==" and "255"
+        (
+            pd.DataFrame(
+                {
+                    "condition_1": ["3", "255"],
+                    "condition_2": ["==2", "!=2"],
+                    "final_class": [1, 2],
+                }
+            ),
+            [
+                "where((condition_2!=2.0), 2, previous)",
+                "where((condition_1==3)&(condition_2==2.0), 1, previous)",
+            ],
+        ),
+    ],
+)
+def test_generate_numexpr_expressions(rules_df, expected_expressions):
+    con_cols = ["condition_1", "condition_2"]
+    class_col = "final_class"
+
+    generated_expressions = generate_numexpr_expressions(
+        rules_df[con_cols + [class_col]], class_col, "previous"
+    )
+
+    def normalize_expression(expression):
+        match = re.match(r"where\((.*), (.*?), (.*?)\)", expression)
+        if match:
+            conditions, true_value, false_value = match.groups()
+            # Split conditions, sort them, and rejoin
+            sorted_conditions = "&".join(sorted(conditions.split("&")))
+            return f"where({sorted_conditions}, {true_value}, {false_value})"
+        return expression
+
+    normalized_expected = [normalize_expression(expr) for expr in expected_expressions]
+    normalized_generated = [
+        normalize_expression(expr) for expr in generated_expressions
+    ]
+
+    assert (
+        normalized_generated == normalized_expected
+    ), f"Expected {expected_expressions}, but got {generated_expressions}"
+
+
+def test_level4(urban_shape):
+
+    level_3_4 = np.array(
+        [
+            [
+                [110, 110, 110, 110, 110, 110],
+                [110, 110, 110, 110, 110, 110],
+                [110, 110, 110, 110, 110, 110],
+                [110, 110, 110, 110, 110, 110],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    cultivated = np.array(
+        [
+            [
+                [111, 111, 111, 111, 111, 111],
+                [111, 111, 111, 111, 111, 111],
+                [112, 112, 112, 112, 112, 112],
+                [112, 112, 112, 112, 112, 112],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    urban = np.array(
+        [
+            [
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    woody = np.array(
+        [
+            [
+                [113, 114, 113, 113, 113, 113],
+                [113, 114, 114, 114, 114, 114],
+                [113, 114, 113, 113, 113, 113],
+                [113, 114, 114, 114, 114, 114],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    pv_pc_50 = np.array(
+        [
+            [
+                [np.nan, np.nan, 66, 41, 21, 5],
+                [3, 66, 41, 21, 5, 3],
+                [np.nan, np.nan, 66, 41, 21, 5],
+                [3, 66, 41, 21, 5, 3],
+            ]
+        ],
+        dtype="float32",
+    )
+
+    water_season = np.array(
+        [
+            [
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+                [255, 255, 255, 255, 255, 255],
+            ]
+        ],
+        dtype="uint8",
+    )
+
+    water_frequency = np.array(
+        [
+            [
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+            ]
+        ],
+        dtype="float32",
+    )
+
+    bs_pc_50 = np.array(
+        [
+            [
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+            ]
+        ],
+        dtype="float32",
+    )
+
+    tuples = [
+        (np.datetime64("2000-01-01T00"), np.datetime64("2000-01-01")),
+    ]
+    index = pd.MultiIndex.from_tuples(tuples, names=["time", "solar_day"])
+
+    affine = Affine.translation(10, 0) * Affine.scale(
+        (20 - 10) / level_3_4.shape[2], (5 - 0) / level_3_4.shape[1]
+    )
+    geobox = GeoBox(
+        crs="epsg:3577",
+        affine=affine,
+        width=level_3_4.shape[2],
+        height=level_3_4.shape[1],
+    )
+    coords = geobox.xr_coords()
+
+    data_vars = {
+        "level_3_4": xr.DataArray(
+            da.from_array(level_3_4, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "artificial_surface": xr.DataArray(
+            da.from_array(urban, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "cultivated": xr.DataArray(
+            da.from_array(cultivated, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "woody": xr.DataArray(
+            da.from_array(woody, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "pv_pc_50": xr.DataArray(
+            da.from_array(pv_pc_50, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "bs_pc_50": xr.DataArray(
+            da.from_array(bs_pc_50, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "water_frequency": xr.DataArray(
+            da.from_array(water_frequency, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+        "water_season": xr.DataArray(
+            da.from_array(water_season, chunks=(1, -1, -1)),
+            dims=("spec", "y", "x"),
+            attrs={"nodata": 255},
+        ),
+    }
+
+    xx = xr.Dataset(data_vars=data_vars, coords=coords)
+    xx = xx.assign_coords(xr.Coordinates.from_pandas_multiindex(index, "spec"))
+
+    expected_l4 = np.array(
+        [
+            [2, 3, 9, 10, 11, 12],
+            [13, 14, 15, 16, 17, 18],
+            [20, 21, 27, 28, 29, 30],
+            [31, 32, 33, 34, 35, 36],
+        ],
+        dtype="uint8",
+    )
+
+    expected_l3 = np.array(
+        [
+            [111, 111, 111, 111, 111, 111],
+            [111, 111, 111, 111, 111, 111],
+            [112, 112, 112, 112, 112, 112],
+            [112, 112, 112, 112, 112, 112],
+        ],
+        dtype="uint8",
+    )
+
+    stats_l4 = StatsLccsLevel4(
+        measurements=["level3", "level4"],
+        class_def_path="s3://dea-public-data-dev/lccs_validation/c3/data_to_plot/"
+        "lccs_colour_scheme_golden_dark_au_c3.csv",
+        class_condition={
+            "level3": ["level1", "artificial_surface", "cultivated"],
+            "level4": [
+                "level1",
+                "level3",
+                "woody",
+                "water_season",
+                "water_frequency",
+                "pv_pc_50",
+                "bs_pc_50",
+            ],
+        },
+        data_var_condition={"level1": "level_3_4"},
+        urban_mask=urban_shape,
+        filter_expression="mock > 9",
+        mask_threshold=0.3,
+    )
+    ds = stats_l4.reduce(xx)
+
+    assert (ds.level3.compute() == expected_l3).all()
+    assert (ds.level4.compute() == expected_l4).all()
