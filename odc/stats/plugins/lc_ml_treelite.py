5807a1966d5feeb955c7ca95667637a55b888bb1
Emma Ai <emma.ai@ga.gov.au>
2025-05-30 06:52:43 +0000

    fix plugin import

--- odc/stats/plugins/lc_ml_treelite.py
+++ odc/stats/plugins/lc_ml_treelite.py
@@ -16,7 +16,7 @@ from dask.distributed import get_worker
 from datacube.model import Dataset
 from odc.geo.geobox import GeoBox
 from odc.algo._memsink import yxbt_sink, yxt_sink
-from odc.algo.io import load_with_native_transform
+from odc.stats.io import load_with_native_transform
 
 from odc.stats._algebra import expr_eval
 from odc.stats.model import DateTimeRange
