eef1c979c0cc3a863b6b96a465f79ecabe84c246
andrewdhicks <andrew.hicks@ga.gov.au>
2026-01-22 23:07:03 +0000

    Handle moved import for stac2ds

--- odc/stats/_stac_fetch.py
+++ odc/stats/_stac_fetch.py
@@ -1,7 +1,11 @@
 import json
 
 from odc.aio import S3Fetcher, s3_find_glob
-from odc.stac.eo3 import stac2ds
+
+try:
+    from datacube.metadata import stac2ds
+except ImportError:
+    from odc.stac.eo3 import stac2ds
 from pystac.item import Item
 
 
